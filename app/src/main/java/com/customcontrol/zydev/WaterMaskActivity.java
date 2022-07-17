a461ae7a107ac4aaf0b1e2e9dd02832494a4bea2
neimengwuyanzu <meicuowoshizhangyu@gmail.com>
2022-07-17 22:49:36 +0800

    总算他妈的有点进展

--- app/src/main/java/com/customcontrol/zydev/WaterMaskActivity.java
+++ app/src/main/java/com/customcontrol/zydev/WaterMaskActivity.java
@@ -8,8 +8,15 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.customcontrol.zydev.util.ImageUtil;
 import com.customcontrol.zydev.util.WaterMaskUtil;
 import com.customcontrol.zydev.weight.WaterMaskView;
+import com.watermark.androidwm_light.WatermarkBuilder;
+import com.watermark.androidwm_light.bean.WatermarkImage;
+import com.watermark.androidwm_light.bean.WatermarkText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WaterMaskActivity extends AppCompatActivity {
 
@@ -33,14 +40,66 @@ public class WaterMaskActivity extends AppCompatActivity {
         ivWaterMark= (ImageView) findViewById(R.id.wartermark_pic);
 
         sourBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bg_demo);
-        waterMaskView = new WaterMaskView(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        waterMaskView.setLayoutParams(params);
+
+        WatermarkImage watermarkImage1 = new WatermarkImage(this, R.mipmap.bg_start_progress);
+        watermarkImage1.setSize(0.05);
+        watermarkImage1.setImageAlpha(255);
+        watermarkImage1.setPositionX(0.05);
+        watermarkImage1.setPositionY(0.75);
+        WatermarkText watermarkText1 = new WatermarkText("位置位置");
+        watermarkText1.setTextColor(R.color.black);
+        watermarkText1.setPositionX(watermarkImage1.getPosition().getPositionX()+0.1);
+        watermarkText1.setPositionY(watermarkImage1.getPosition().getPositionY());
+        watermarkText1.setTextSize(15.0);
+        watermarkText1.setTextAlpha (255);
+
+
+
+        WatermarkImage watermarkImage2 = new WatermarkImage(this, R.mipmap.bg_start_progress_run);
+        watermarkImage2.setSize(0.05);
+        watermarkImage2.setImageAlpha(255);
+        watermarkImage2.setPositionX(0.05);
+        watermarkImage2.setPositionY(0.85);
+        WatermarkText watermarkText2 = new WatermarkText("名称名称名称");
+        watermarkText2.setTextColor(R.color.black);
+        watermarkText2.setPositionX(watermarkText1.getPosition().getPositionX());
+        watermarkText2.setPositionY(watermarkImage2.getPosition().getPositionY());
+        watermarkText2.setTextSize(15.0);
+        watermarkText2.setTextAlpha (255);
+
+
+
+
+
+
+        List<WatermarkText> textList = new ArrayList<>();
+        textList.add(watermarkText1);
+        textList.add(watermarkText2);
+        List<WatermarkImage> imageList = new ArrayList<>();
+        imageList.add(watermarkImage1);
+        imageList.add(watermarkImage2);
+
+
+
+
+        Bitmap bitmap = WatermarkBuilder
+                .create(this, sourBitmap)
+                .loadWatermarkTexts(textList)
+                .loadWatermarkImages(imageList)
+                .getWatermark()
+                .getOutputImage();
+        ivWaterMark.setImageBitmap(bitmap);
+//        waterMaskView = new WaterMaskView(this);
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT*2, RelativeLayout.LayoutParams.MATCH_PARENT);
+//        waterMaskView.setLayoutParams(params);
 //        waterMaskView.setCompanyText("XXXXXX公司");
 //        waterMaskView.setInfoText("这是相关信息1这是相关信息2这是相关信息3这是相关信息4这是相关信息5");
-
+//        Bitmap testBitmap = ImageUtil.drawTextToLeftTop(this, sourBitmap, "示例文字", 30, R.color.black, 20, 30);
+//        Bitmap testBitmap = ImageUtil.drawTextToLeftTop(this, sourBitmap, "示例文字", 30, R.color.black, 20, 30);
+//        Bitmap testBitmap = ImageUtil.drawTextToLeftBottom(this, sourBitmap, "示例文字", 10, R.color.black, 10, 10);
+//
         //默认设置水印位置在左下
-        saveWaterMask(LEFT_BOTTOM);
+//        saveWaterMask(LEFT_BOTTOM);
 
 
     }
--- app/src/main/java/com/customcontrol/zydev/util/WaterMaskUtil.java
+++ app/src/main/java/com/customcontrol/zydev/util/WaterMaskUtil.java
@@ -155,7 +155,7 @@ public class WaterMaskUtil {
      */
     public static Bitmap convertViewToBitmap(View view){
         view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.layout(view.getMeasuredWidth(), view.getMeasuredHeight(), view.getMeasuredWidth(), view.getMeasuredHeight());
         view.buildDrawingCache();
         Bitmap bitmap = view.getDrawingCache();
         return bitmap;
